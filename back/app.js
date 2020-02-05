133ae2c5a7795f936fa394d1f380199795fc5bf4
TMoretau <tmoretau@gmail.com>
2020-02-05 14:27:16 +0100

    added back

---
+++ back/app.js
@@ -0,0 +1,33 @@
+// RGAPI-ccdb30eb-5b24-477e-a8d1-d28f8a61c4ff
+
+// in sublime
+const express = require('express');
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/dev', cors(), function (req, res, next) {
+	const region = req.query.region;
+	const query = req.query.query;
+	const url = 'https://' + region + '.api.riotgames.com' + query;
+	const headers = {
+		"X-Riot-Token": "RGAPI-ccdb30eb-5b24-477e-a8d1-d28f8a61c4ff",
+	};
+	axios.get(url, { headers })
+		.then((resp) => {
+			return res.status(200).send(resp.data);
+		})
+		.catch((err) => {
+			console.warn(err);
+			return res.status(500).send();
+		});
+});
+
+app.listen(port, function () {
+ console.log('Example app listening on port ' + port + '!');
+});
