3a076c5ead469a208fd389d16a89b32cfc23798d
alexis <lagniel.alexis@gmail.com>
2020-02-06 14:14:00 +0100

    changing linter config

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,12 +1,29 @@
 module.exports = {
-    "rules": {
-        "no-console": "off",
-        "no-restricted-syntax": [
-            "error",
-            {
-                "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
-                "message": "Unexpected property on console object was called"
-            }
-        ]
-    }
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        message: 'Unexpected property on console object was called',
+      },
+    ],
+    'prefer-destructuring': ['error', {
+      array: false,
+      object: false,
+    }, {
+      enforceForRenamedProperties: false,
+    }],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 };
