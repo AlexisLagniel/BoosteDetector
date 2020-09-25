e77bd0d9a3a8543f14cf34f61834c0a4d6c4f224
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2020-09-25 23:24:22 +0200

    changing api key

--- back/app.js
+++ back/app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const token = 'RGAPI-ca49cba5-3332-4c60-a055-e2136351749f';
+const token = 'RGAPI-6c012518-bf38-4016-8638-c9103b0e0231';
 
 app.use(cors());
 
